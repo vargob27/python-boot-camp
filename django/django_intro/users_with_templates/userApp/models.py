d5f16eb419c53fb6b3a07131dd4ea58a7033d737
vargob27 <80635028+vargob27@users.noreply.github.com>
2021-06-01 18:13:17 -0400

    Created/Updated Assignment

--- django/django_intro/users_with_templates/userApp/models.py
+++ django/django_intro/users_with_templates/userApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    firstName = models.TextField(max_length=255)
+    lastName = models.TextField(max_length=255)
+    email = models.TextField(max_length=255)
+    age = models.IntegerField(max_length=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- django/django_intro/users_with_templates/userApp/views.py
+++ django/django_intro/users_with_templates/userApp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+def index(request):
+    context = {
+        "all_users": User.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def add(request):
+    
+    return redirect('/')
